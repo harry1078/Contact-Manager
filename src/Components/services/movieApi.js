12c7227c945771faf40f171632e2002cb5e56896
arjun singh <arjun.singh@gmail.com>
2023-06-20 20:01:15 +0530

    minor changes

--- src/Components/services/movieApi.js
+++ src/Components/services/movieApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const BASE_URL = "http://www.omdbapi.com";
-const API_KEY = "ef566ba6";
+const API_KEY = "1111111";
 export const movieApi = createApi({
   reducerPath: "movieApi",
   baseQuery: fetchBaseQuery({ baseUrl: `${BASE_URL}` }),
